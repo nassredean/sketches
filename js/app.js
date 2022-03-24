38f2b0280a5ea5ab5f1e45c2e046ccb6877c10c8
Nassredean Nasseri <dean@nasseri.io>
2022-03-23 23:06:06 -0400

    sketch boilerplate

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 import "../css/style.css";
+import p5 from "p5";
 
-const app = () => {};
+const s = (sketch) => {
+  let x = 100;
+  let y = 100;
 
-app();
+  sketch.setup = () => {
+    sketch.createCanvas(200, 200);
+  };
+
+  sketch.draw = () => {
+    sketch.background(0);
+    sketch.fill(255);
+    sketch.rect(x, y, 50, 50);
+  };
+};
+
+let myp5 = new p5(s);
